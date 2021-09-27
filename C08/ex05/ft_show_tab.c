393aa5b23f5b5591f4914732523c8d6a34149982
김건엽 <yeopto@YeopBookAir.local>
2021-09-27 11:06:15 +0900

    backup

--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -6,7 +6,7 @@
 /*   By: gunykim <gunykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 18:47:43 by gunykim           #+#    #+#             */
-/*   Updated: 2021/09/27 00:43:02 by gunykim          ###   ########.fr       */
+/*   Updated: 2021/09/27 11:05:51 by gunykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,27 +22,31 @@ void	ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
 	{
-		write(1, "-2147483648", 11);
-		return ;
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
 	}
-	if (nb < 0)
+	else if (nb < 0)
 	{
 		ft_putchar('-');
-		nb = -nb;
+		ft_putnbr(-nb);
 	}
-	if (nb > 9)
-		ft_putnbr(nb / 10);
-	ft_putchar(nb % 10 + '0');
+	else
+	{
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+		}
+		ft_putchar(nb % 10 + 48);
+	}
 }
 
 void	ft_putstr(char *str)
 {
-	int	n;
-
-	n = 0;
-	while (str[n])
-		write(1, &str[n++], 1);
-	write(1, "\n", 1);
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
 }
 
 void	ft_show_tab(struct s_stock_str *par)
